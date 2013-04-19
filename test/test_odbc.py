c38c4d8ebe646e66d48bef25efce31ad9f65d0ba
pdm <ppppdm@gmail.com>
2013-04-19 15:16:55 +0800

    test done test_odbc.py, insert and select ok

--- test/test_odbc.py
+++ test/test_odbc.py
@@ -3,20 +3,50 @@
 # email : ppppdm@gmail.com
 
 import pyodbc
-#help(pyodbc.Cursor)
+import datetime
+
+HOST = '10.20.1.200' # '210.73.152.201'
+USER = 'sa'
+PWD = 'sa'
+DATABASE = 'CDMTCP'
+
+
+
+
+help(pyodbc.Cursor.execute)
 try:
-    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=10.20.1.129;DATABASE=bus2;UID=sa;PWD=sa')
+    conn = pyodbc.connect('DRIVER={SQL Server}', host = HOST, user = USER, password = PWD, database = DATABASE)
+    #conn = pyodbc.connect('DRIVER={SQL Server};SERVER=10.20.1.200;DATABASE=CDMTCP;UID=sa;PWD=sa')
     print(conn)
     cur = conn.cursor()
-    cur.execute('sp_helpdb')
+    cur.execute('sp_help tbl_heartbeatinfo')
+    # cur.execute('sp_helpdb CDMTCP')
     for i in cur.fetchall():
         print(i)
-    #cur.execurt('select * from tbl_heartbeatinfo')
-    #for i in cur.fetchall():
-    #    print(i)
     
-    cur.close()
+    ID = 'newid()'
+    camera_id = 'E2C34D5E992F'
+    x = '5678.12345'
+    y = '12345.67891'
+    gpstime = datetime.datetime.now()
+    road = ''
+    mph = 25
+    
+    cur.execute("INSERT INTO tbl_heartbeatinfo( ID, camera_id, gpx, gpy, gpstime, roadname, mph) VALUES (newid(), ?, ?, ?, ?, ?, ?)", 
+                (camera_id, x, y, gpstime, road, mph))
+    #cur.execute("INSERT INTO tbl_heartbeatinfo( ID ,camera_id, gpx, gpy, gpstime, roadname, mph) VALUES (newid(),'E2C34D5E992F', '5678.12345', '12345.67891', 2013-4-19, '', 25)")
+    conn.commit()
+    cur.execute('select * from tbl_heartbeatinfo')
+    for i in cur.fetchall():
+        print(i)
+    
+    
+    
+    
+    #cur.close()
     conn.close()
     print(conn)
 except Exception as e:
-    print(print(e.args[1]))
+    print(e)
+    conn.close()
+    print(conn)
