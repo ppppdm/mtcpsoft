77fbf0fbf4e15d96ffa819474579750c5c8d43b8
pdm <ppppdm@gmail.com>
2013-04-24 17:05:48 +0800

    add polling fun for file watcher, not test

--- imgInfoGeter/fileWatcher.py
+++ imgInfoGeter/fileWatcher.py
@@ -4,10 +4,21 @@
 
 import threading
 import os
-
+import time
 
 # using Polling , can work across platform
+def watchFileChange_cross(path_to_watch, func_handle_change = None):
 
+    path_to_watch = "."
+    before = dict ([(f, None) for f in os.listdir (path_to_watch)])
+    while 1:
+        time.sleep (5)
+        after = dict ([(f, None) for f in os.listdir (path_to_watch)])
+        added = [f for f in after if not f in before]
+        removed = [f for f in before if not f in after]
+        if added: print ("Added: ", ", ".join (added))
+        if removed: print( "Removed: ", ", ".join (removed))
+        before = after
 
 
 
