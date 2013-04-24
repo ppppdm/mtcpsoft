dc0dc92f82f3b9f0d0d3fea4729c83ed2408c835
pdm <ppppdm@gmail.com>
2013-04-24 20:06:37 +0800

    add log to InfoProcess.py

--- imgInfoGeter/InfoProcess.py
+++ imgInfoGeter/InfoProcess.py
@@ -1,7 +1,13 @@
 # -*- coding:gbk -*-
 # auther : pdm
 # email : ppppdm@gmail.com
+import logging
+import logging.config
 
+logging.config.fileConfig("logging.conf")
+
+#create logger
+logger = logging.getLogger("example")
 
 # self module
 
@@ -19,11 +25,6 @@ INFO_ITEM_LEN = [12, 17, 10, 11, 5,
                  1, 1
                  ]
 
-OLD_INFO_ITEM_LEN = [12, 16, 10, 11, 5, 
-                 2, 16, 8, 2, 4, 
-                 1, 1
-                 ]
-
 # read file and get info in the image
 # before the info we want there is 6 bytes file head
 # after that is the info we want, the item of info see
@@ -36,11 +37,17 @@ def store_infos(infos):
 def get_infos(f):
     infos = []
     
+    # in test print the info
     f.seek(BEFORE_INFO_LEN)
-    # just use old for test
+    b_data = f.read(INFO_LEN)
+    logger.debug(b_data)
+    #
+    
+    
+    f.seek(BEFORE_INFO_LEN)
     
     for i in INFO_ITMES:
-        item_len = OLD_INFO_ITEM_LEN[INFO_ITMES.index(i)]
+        item_len = INFO_ITEM_LEN[INFO_ITMES.index(i)]
         b_data = f.read(item_len)
         try:
             if i == 'CAR LICENSE':
@@ -50,8 +57,9 @@ def get_infos(f):
             if i == 'X' or i == 'Y':
                 b_data = b_data[:-1]
             infos.append((i, str(b_data, 'gbk')))
-        except:
-            print('decode item %s error!'%(i))
+        except Exception as e:
+            print('decode item %s error!'%(i), e)
+            logger.debug(e)
         
     print(infos)
     
@@ -68,8 +76,9 @@ def file_process(file):
         store_infos(infos)
     
         f.close()
-    except:
-        print('file ', f, 'open error!')
+    except Exception as e:
+        print('file ', f, 'open error!', e)
+        logger.debug(e)
     
     return
 
