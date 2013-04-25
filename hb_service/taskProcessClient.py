30ae6b0a9cc73c345bf93581049471c20f6df940
pdm <ppppdm@gmail.com>
2013-04-25 17:27:23 +0800

    add file taskProcessClient.py & ..Server.py, for taskProcess

---
+++ hb_service/taskProcessClient.py
@@ -0,0 +1,21 @@
+# -*- coding:gbk -*-
+# auther : pdm
+# email : ppppdm@gmail.com
+
+
+def sendCmdToServer(ip_list, arg_list):
+    
+    # connect to server
+    
+    
+    # send to server
+    
+    
+    # close connect
+    
+    
+    return False
+
+
+if __name__=='__main__':
+    print(__file__, 'test')
