7e41a64aebca7b3387cfe5c37d158a2627d3888f
pdm <ppppdm@gmail.com>
2013-04-27 17:42:28 +0800

    add code for insert data to db imgInfo, not ok, need fix

--- imgInfoGeter/InfoProcess.py
+++ imgInfoGeter/InfoProcess.py
@@ -4,6 +4,8 @@
 import logging
 import logging.config
 import traceback
+import datetime
+
 
 logging.config.fileConfig("logging.conf")
 
@@ -33,7 +35,7 @@ INFO_ITEM_LEN = [12, 17, 10, 11, 5,
 # the INFO_ITMES, each item's len see the INFO_ITEM_LEN
 
 
-def store_infos(infos):
+def store_infos(infos, file):
     
     # store to logfile
     logger.debug(infos)
@@ -41,28 +43,81 @@ def store_infos(infos):
     # store to db
     db_conn = dbManager.get_db_connect()
     if db_conn:
+        '''
         try:
-            cur = db_conn.cursor()
-            cur.execute('SELECT count(*) from LS_pictures')
-            #cur.execute("INSERT INTO tbl_heartbeatinfo( ID, camera_id, gpx, gpy, gpstime, roadname, mph, createtime) VALUES (newid(), ?, ?, ?, ?, ?, ?, ?)", 
-            #    (camera_id, x, y, gpstime, road, mph, createtime))
-        except:
-            print('db execute error!')
+            cur                = db_conn.cursor()
+            
+            camera_id          = infos.get('MAC', '')
+            picture_name       = file
+            x                  = infos.get('X', '')
+            y                  = infos.get('Y', '')
+            collect_date1      = infos.get('RTC', '')
+            direction          = infos.get('DIRECT', '')
+            car_id             = infos.get('CAR LICENSE', '')
+            license_color      = infos.get('LICENSE COLOR', '')
+            captrue_serial_num = infos.get('SERIAL NUMBER', '')
+            minor_captrue_num  = infos.get('NO.', '')
+            flag1              = infos.get('CAPTURE FALG', '')
+            
+            print(camera_id)
+            
+            if collect_date1 != '':
+                collect_date1 = datetime.datetime.strptime(collect_date1, '%Y%m%d%H%M%S%f')
+            else:
+                collect_date1 = None
+            
+            #
+            cur.execute("""INSERT INTO LS_pictures( camera_id, picture_name) VALUES (?, ?)""", 
+                (
+                camera_id, 
+                picture_name
+                ))
+        except Exception as e:
+            print('db execute error!', e)
             logger.debug('db execute error!')
         
         try:
             db_conn.commit()
-        except:
-            print('db commit error!')
+            
+            
+            print(cur.execute('select * from LS_pictures where camera_id=08002812d137').fetchall())
+        except Exception as e:
+            print('db commit error!', e)
             logger.debug('db commit error!')
-        
+        '''
     dbManager.close_db_connect(db_conn)
     
     return
 
-
+'''
+, 
+                picture_name, 
+                gps_x, 
+                gps_y, 
+                collect_date1, 
+                direction, 
+                car_id, 
+                license_color, 
+                captrue_serial_num, 
+                minor_captrue_num, 
+                flag1)
+                
+                , ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+                
+                , 
+                picture_name, 
+                x, 
+                y, 
+                collect_date1, 
+                direction,
+                car_id, 
+                license_color, 
+                captrue_serial_num, 
+                minor_captrue_num, 
+                flag1
+'''
 def get_infos(f):
-    infos = []
+    infos = {}
     
     # in test print the info
     f.seek(BEFORE_INFO_LEN)
@@ -83,7 +138,8 @@ def get_infos(f):
                 b_data = b_data[:2]
             if i == 'X' or i == 'Y':
                 b_data = b_data[:-1]
-            infos.append((i, str(b_data, 'gbk')))
+            #infos.append((i, str(b_data, 'gbk')))
+            infos[i] = str(b_data, 'gbk')
         except Exception as e:
             print('decode item %s error!'%(i), e)
             logger.debug(e)
@@ -100,7 +156,7 @@ def file_process(file):
     
         infos = get_infos(f)
     
-        store_infos(infos)
+        store_infos(infos, file)
     
         f.close()
     except Exception as e:
@@ -118,8 +174,12 @@ if __name__=='__main__':
     for i in INFO_ITEM_LEN:
         t += i
     print(t)
-    
+    '''
     TEST_FILES = ['../res/1.jpg', '../res/2013041710510982-d137-0001-3.jpg']
     for i in TEST_FILES:
         file_process(i)
+    '''
+    # test insert to db
+    infos = {'MAC': '08002812d137', 'CAR DISTENCE': '40', 'RTC': '20130417105109823', 'SERIAL NUMBER': '001\x02', 'Y': '1848.3899,', 'X': '157.7773,', 'CAR LICENSE': 'ÕA0D928\x00'}
+    store_infos(infos, '../res/2013041710510982-d137-0001-3.jpg')
     
