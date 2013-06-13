7575c8b87e899650fae4a2c4fefaafcdd33ae148
ppppdm <ppppdm@gmail.com>
2013-06-13 21:18:05 +0800

    add test_update_hbnew.py for test update to hbinfonew db table, not use yet

---
+++ test/test_update_hbnew.py
@@ -0,0 +1,64 @@
+# -*- coding:gbk -*-
+# auther : pdm
+# email : ppppdm@gmail.com
+
+# global variabls
+DB_HOST = '10.20.1.129' # '210.73.152.201'
+USER = 'sa'
+PWD = 'skcl@2013'
+DATABASE = 'CDMTCP'
+
+import datetime
+
+try:
+    import pyodbc
+except:
+    print('no module pyodbc, should init first!')
+
+def get_db_connect():
+    db_conn = None
+    try:
+        db_conn = pyodbc.connect('DRIVER={SQL Server}', host = DB_HOST, user = USER, password = PWD, database = DATABASE)
+    except: # not print db execption yet
+        #logger.debug('init db got an error!')
+        print('init db got an error!')
+    return db_conn
+
+def close_db_connect(db_conn):
+    if db_conn:
+        db_conn.close()
+
+def update_hbnew(camera_id):
+    conn = get_db_connect()
+    #print('autocommit :', conn.autocommit)
+    if conn:
+        cur = conn.cursor()
+        gpx = 110.253
+        gpy = 31.021
+        gpstime = datetime.datetime.now()
+        roadname = ''
+        mph = 0
+        createtime = datetime.datetime.now()
+        
+        sql = "update tbl_heartbeatinfo_new set gpx = ?, gpy = ?, gpstime = ?, roadname = ?, mph = ?, createtime = ? where (camera_id = ?)"
+        try:
+            cur.execute(sql, gpx, gpy, gpstime, roadname, mph, createtime, camera_id)
+        except:
+            print('db execute error')
+        
+        try:
+            conn.commit()
+        except:
+            print('db commit error')
+        else:
+            print('db update success')
+        
+        
+    
+    close_db_connect(conn)
+    return
+
+if __name__=='__main__':
+    print(__file__, 'test')
+    update_hbnew('123456')
+    
